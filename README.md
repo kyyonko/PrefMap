1e44684589b5cb76c30be28f1a9e5894ac3619a9
kyyonko <kyyonko@gmail.com>
2016-07-04 00:17:43 +0900

    modify a little

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PrefMap
 都道府県ごとの各年齢層の人口に占める割合を可視化する簡単なShinyアプリケーション
+https://kyyonko.shinyapps.io/shiny/
+
+
--- ui.R
+++ ui.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(ggplot2)
+library(leaflet) 
 
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
